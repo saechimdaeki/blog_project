cb6b93ab72e6f93bc3c5e766eabbff4783b6e9b4
Junseong kim <anima94@kakao.com>
2022-06-21 12:55:55 +0900

    project init

---
+++ saechimlog/src/main/kotlin/com/saechim/saechimlog/SaechimLogApplication.kt
@@ -0,0 +1,11 @@
+package com.saechim.saechimlog
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SaechimLogApplication
+
+fun main(args: Array<String>) {
+    runApplication<SaechimLogApplication>(*args)
+}
---
+++ saechimlog/src/test/kotlin/com/saechim/saechimlog/SaechimLogApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.saechim.saechimlog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SaechimLogApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
